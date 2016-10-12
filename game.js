8cd82046b9cafd164504e368931856c2b946a00a
Steven Freeman <sfreeman422@gmail.com>
2016-10-12 12:11:36 -0400

    game.js chooses a word for the user now

---
+++ game.js
@@ -0,0 +1,8 @@
+var words = ["nas", "biggie", "tupac", "big\xa0l", "a\xa0tribe\xa0called\xa0quest", "snoop\xa0dogg", "dr\xa0dre", "nwa", "wu\xa0tang\xa0clan", "beastie\xa0boys", "method\xa0man", "jay-z", "busta\xa0rhymes", "big\xa0pun", "mos\xa0def", "ludacris", "run\xa0dmc", "gang\xa0starr", "ice\xa0cube", "mobb\xa0deep"];
+
+function chooseWord(){
+	var randNum = Math.floor((Math.random()*words.length)+1);
+	return words[randNum];
+}
+
+console.log(chooseWord());
