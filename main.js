8a785278718b9f106f532c2c4e9e9c303ddd4c30
Steven Freeman <sfreeman422@gmail.com>
2016-10-12 12:24:04 -0400

    Updated comments on each file for clarity purposes

--- main.js
+++ main.js
@@ -1,2 +1,4 @@
+//Requires that the game.js file is loaded in order to use this JS file. We need this in order to choose a word. 
 var game = require('./game.js');
+//Console logs the word using the return value of game.chooseWord(). The chooseWord function exists in the game.js file. 
 console.log(game.chooseWord());
